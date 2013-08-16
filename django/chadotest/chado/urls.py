6cf9ac6523f2f6ba1c5a0979083136d427501d12
Alan Cleary <alancleary@comcast.net>
2013-08-16 14:01:38 -0600

    Touched up the urls

--- django/chadotest/chado/urls.py
+++ django/chadotest/chado/urls.py
@@ -13,7 +13,6 @@ urlpatterns = patterns('chado.views',
     url(r'^msa/(?P<feature_id>\d+)/$', 'msa_view', {'template_name' : 'chado/msa/view.html'}, name='msa_view'),
     url(r'^msa/(?P<feature_id>\d+)/consensus/$', 'msa_consensus', {'template_name' : 'chado/msa/consensus.html'}, name='msa_consensus'),
     url(r'^msa/(?P<feature_id>\d+)/download/$', 'msa_consensus_download', name='msa_consensus_download'),
-    #url(r'^msa/(?P<feature_id>\d+)/download/$', 'msa_download', {}, name='msa_download'),
 
     # tree
     url(r'^phylo/$', 'phylo_index', {'template_name' : 'chado/phylo/index.html'}, name='phylo_index'),
@@ -21,7 +20,6 @@ urlpatterns = patterns('chado.views',
     url(r'^phylo/(?P<phylotree_id>\d+)/newick/$', 'phylo_newick', {'template_name' : 'chado/phylo/newick.html'}, name='phylo_newick'),
     url(r'^phylo/(?P<phylotree_id>\d+)/download/$', 'phylo_newick_download', name='phylo_newick_download'),
     url(r'^phylo/node/(?P<phylonode_id>\d+)/gff_download/$', 'phylo_gff_download', name='phylo_gff_download'),
-    url(r'^phylo/sample/$', 'phylo_sample', {'template_name' : 'chado/phylo/sample.html'}, name='phylo_sample'),
 
     # feature
     url(r'^feature/(?P<feature_id>\d+)/$', 'feature_view', {'template_name' : 'chado/feature/view.html'}, name='feature_view'),
