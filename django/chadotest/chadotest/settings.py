745ef54d1f418be176a324b501612ac535390b39
Alan Cleary <alancleary@comcast.net>
2014-06-20 13:51:05 -0600

    Set back to correct database

--- django/chadotest/chadotest/settings.py
+++ django/chadotest/chadotest/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chado_test',                      # Or path to database file if using sqlite3.
+        'NAME': 'chado',                      # Or path to database file if using sqlite3.
         'USER': 'chado',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
