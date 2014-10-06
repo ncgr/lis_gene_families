f366b0d149d57e235c22cf4bb3095fd35a4ba2be
Andrew <adf@ncgr.org>
2014-10-06 12:58:11 -0600

    no db params set for commit

--- django/chadotest/chadotest/settings.py
+++ django/chadotest/chadotest/settings.py
@@ -19,15 +19,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': '8-15-13_fixed-msas',                      # Or path to database file if using sqlite3.
-        #'NAME': 'chado_peu_gene_trees',                      # Or path to database file if using sqlite3.
-        'NAME': 'drupal_isu_20140903',                      # Or path to database file if using sqlite3.
-        #'USER': 'chado_readonly',
-        #'PASSWORD': 'tlnir@ng0',
-        'USER': 'adf',
-        'PASSWORD': 'changeme',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
