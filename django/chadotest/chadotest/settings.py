d94fbed1c5e4f99c2dd4a8f26bd88d31bb0d19cc
Alan Cleary <alancleary@comcast.net>
2014-11-09 13:25:16 -0700

    Removed local db settings

--- django/chadotest/chadotest/settings.py
+++ django/chadotest/chadotest/settings.py
@@ -19,8 +19,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'context_test',                      # Or path to database file if using sqlite3.
-        'USER': 'alan',
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
