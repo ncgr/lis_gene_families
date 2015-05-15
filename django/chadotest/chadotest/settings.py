d62ba7790e3fb526229144be88fef19d07c7b6d3
Andrew Farmer <adf@ncgr.org>
2015-05-15 17:40:40 -0500

    settings appropriate for legumeinfo servers. no secrets here

--- django/chadotest/chadotest/settings.py
+++ django/chadotest/chadotest/settings.py
@@ -19,11 +19,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chado',                      # Or path to database file if using sqlite3.
-        'USER': 'chado',
+        'NAME': 'drupal',                      # Or path to database file if using sqlite3.
+        'USER': 'www',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PORT': os.environ['PGPORT'],                      # Set to empty string for default.
     }
 }
 
