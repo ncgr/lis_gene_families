9efa39e145edfc35e6a2318f6973ebb391793e3a
Alan Cleary <alancleary@comcast.net>
2015-07-27 16:09:02 -0600

    Prepared context code for code review.

--- django/chadotest/static/js/context_viewer.js
+++ django/chadotest/static/js/context_viewer.js
@@ -1,7 +1,4 @@
-    
-var viewer;
-
-function context_viewer( container_id, color, data, optional_parameters ) {
+function context_viewer(container_id, color, data, optional_parameters) {
   // clear the contents of the target element first
   document.getElementById(container_id).innerHTML = "";
   
@@ -50,15 +47,15 @@ function context_viewer( container_id, color, data, optional_parameters ) {
     var group_y = 0;
     for (var id in partitions) {
       var partition_groups = [];
-      // sort the partitions by "finish time"
+      // sort the partition groups by "finish time"
       partitions[id].sort(function (a, b) {
         return a[a.length-1].x-b[b.length-1].x;
       });
       // generate the merged tracks
-      while (partitions[ id ].length > 0) {
+      while (partitions[id].length > 0) {
         var track_genes = [];
         var remove = [];
-        for (var i=0; i<partitions[id].length; i++) {
+        for (var i = 0; i < partitions[id].length; i++) {
           // make sure the genes are ordered by x coordinate
           partitions[id].sort(function (a, b) {
             return a.x-b.x;
@@ -72,7 +69,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
           }
         }
         // remove the tracks that were merged
-        for (var i=remove.length-1; i>=0; i--) {
+        for (var i = remove.length-1; i >= 0; i--) {
           partitions[id].splice(remove[i], 1);
         }
         // save the new group
@@ -85,7 +82,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
         return b.genes.length-a.genes.length;
       });
       // add the new groups to the data
-      for (var i=0; i<partition_groups.length; i++) {
+      for (var i = 0; i < partition_groups.length; i++) {
         partition_groups[i].genes = partition_groups[i].genes.map(
                                     function (gene) {
                                       gene.y = group_y;
@@ -113,7 +110,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
       left_pad = 250,
       right_pad = 150,
       num_tracks = data.groups.length,
-      num_genes = get_track_length( data ),
+      num_genes = get_track_length(data),
       h = num_tracks*30+bottom_pad+top_pad,
       min_x = d3.min(data.groups, function (group) {
         return d3.min(group.genes, function (gene) {
@@ -127,7 +124,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
       });
   
   // define the scatter plot
-  viewer = d3.select("#"+container_id)
+  var viewer = d3.select("#"+container_id)
     .append("svg")
     .attr("width", w)
     .attr("height", h);
@@ -146,18 +143,18 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   	// add the group to the y-axis
   	tick_values.push(i);
   
-  	// make groups for the genes
+  	// make svg groups for the genes
   	var selector = ".gene-"+i.toString();
   	var gene_groups = viewer.selectAll(selector)
   	  .data(data.groups[i].genes)
   	  .enter()
   	  .append("g")
   	  .attr("class", "gene")
-  	  .attr("transform", function (e) {
-  	    return "translate("+x(e.x)+", "+y(e.y)+")";
+  	  .attr("transform", function (d) {
+  	    return "translate("+x(d.x)+", "+y(d.y)+")";
   	  });
   
-  	// add genes to the groups
+  	// add genes to the svg groups
   	gene_groups.append("path")
   	  .attr("d", d3.svg.symbol().type("triangle-up").size(200))
   	  .attr("class", function (d) {
@@ -168,7 +165,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   	  	  return "point no_fam";
   	  	} else if (family_sizes[d.family] == 1 && 
                    optional_parameters.selective_coloring !== undefined &&
-                   optional_parameters.selective_coloring) {
+                   optional_parameters.selective_coloring == true) {
   	  	  return "point single";
   	  	} return "point";
       })
@@ -178,7 +175,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   	  .style("fill", function (d) {
   	  	if (d.family == '' ||
             (optional_parameters.selective_coloring !== undefined &&
-             optional_parameters.selective_coloring &&
+             optional_parameters.selective_coloring == true &&
              family_sizes[d.family] == 1)) {
   	  	  return "#ffffff";
   	  	} return color(d.family);
@@ -186,14 +183,14 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   	  .style("cursor", "pointer");
   
   	gene_groups.on("mouseover", function (d) {
-  	     show_tips(d3.select(this));
+  	    show_tips(d3.select(this));
   	  })
   	  .on("mouseout", function (d) {
   	    hide_tips(d3.select(this));
   	  })
   	  .on('click', function (d) {
         if (optional_parameters.gene_clicked !== undefined) {
-  	  	   gene_clicked(d);
+  	  	   optional_parameters.gene_clicked(d);
         }
   	  });
   
@@ -202,12 +199,12 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   	  .attr("class", "tip")
   	  .attr("transform", "translate(3, -14) rotate(-45)")
   	  .attr("text-anchor", "left")
-  	  .text(function (e) {
-  	    return e.name+": "+e.fmin+" - "+e.fmax;
+  	  .text(function (d) {
+  	    return d.name+": "+d.fmin+" - "+d.fmax;
   	  });
   
   	// helper that draws lines between two given genes
-    function drawLine(a, b) {
+    function draw_line(a, b) {
   	  var length = x(a.x)-x(b.x);
   	  var rail_group = viewer.append("g")
   	    .attr("class", "rail")
@@ -221,22 +218,19 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   	      }
   	      return [b.fmin-a.fmax];
   	    });
-  
   	  rail_group.append("line")
   	  	.attr("class", "line")
   	  	.attr("x1", 0)
   	  	.attr("x2", length)
   	  	.attr("y1", 0)
   	  	.attr("y2", y(a.y)-y(b.y));
-  
   	  rail_group.append("text")
   	  	.attr("class", "tip")
   	  	.attr("transform", "translate("+(length/2)+", 10) rotate(45)")
   	  	.attr("text-anchor", "left")
-  	  	.text(function (e) {
+  	  	.text(function (d) {
   	      return rail_group.data();
   	  	});
-  
   	  rail_group.moveToBack();
     }
   
@@ -245,12 +239,12 @@ function context_viewer( container_id, color, data, optional_parameters ) {
     var partition = false;
   	gene_groups.each(function (d) {
   	  var closest;
-  	  var neighbors = gene_groups.filter(function (e) {
-  	  	return e.y == d.y;
+  	  var neighbors = gene_groups.filter(function (n) {
+  	  	return n.y == d.y;
   	  });
-  	  neighbors.each(function (e) {
-  	  	if (e.x < d.x && (closest === undefined || e.x > closest.x)) {
-  	  	  closest = e;
+  	  neighbors.each(function (n) {
+  	  	if (n.x < d.x && (closest === undefined || n.x > closest.x)) {
+  	  	  closest = n;
   	  	}
   	  });
   	  if (closest !== undefined) {
@@ -259,17 +253,17 @@ function context_viewer( container_id, color, data, optional_parameters ) {
             optional_parameters.inter_track == true) {
   	      if (end_genes[d.name] !== undefined && end_genes[d.name].y != d.y) {
             partition = true;
-            drawLine(closest, end_genes[d.name]);
+            draw_line(closest, end_genes[d.name]);
           }
           if (!partition && begin_genes[d.name] === undefined &&
               end_genes[closest.name] === undefined) {
             // inner-track line
-            drawLine(d, closest);
+            draw_line(d, closest);
           }
   	      if (begin_genes[closest.name] !== undefined &&
               begin_genes[closest.name].y != closest.y) {
             partition = false;
-            drawLine(d, begin_genes[closest.name]);
+            draw_line(d, begin_genes[closest.name]);
   	      }
           if (partition) {
             // don't display genes that appear in other partitions
@@ -280,7 +274,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
             }
           } 
         } else {
-          drawLine(d, closest);
+          draw_line(d, closest);
         }
       }
   	});
@@ -296,7 +290,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
   var width = d3.scale.linear()
     .domain([min_width, max_width])
     .range([.1, 5]);
-  rail_groups.attr("stroke-width", function (e) { return width(e); });
+  rail_groups.attr("stroke-width", function (d) { return width(d); });
   
   // construct the y-axes
   var yAxis_left = d3.svg.axis().scale(y).orient("left")
@@ -385,7 +379,7 @@ function context_viewer( container_id, color, data, optional_parameters ) {
       var rail_selection = rail_groups.filter(function (e) {
       	return d3.select(this).attr("y") == y;
       });
-      // will called if optional_parameters.right_axis_clicked !== undefined
+      // called if optional_parameters.right_axis_clicked !== undefined
       optional_parameters.right_axis_clicked(d, gene_selection,
                                              rail_selection);
     });
