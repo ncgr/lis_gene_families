7ec074cc738875f83afb83b86246df2872e8ed4d
Andrew Farmer <adf@ncgr.org>
2015-09-01 01:13:05 -0500

    read SECRET_KEY from separate file, so we can commit this settings file with less danger

--- django/chadotest/chadotest/settings.py
+++ django/chadotest/chadotest/settings.py
@@ -100,7 +100,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'jkdhfwh97649r279whdgfw6476!@%^!&^#%*#&@(*HBQDS'
+SECRET_KEY = open(os.path.join(os.path.dirname(__file__),'secret_key.txt')).read()
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
